4679918b8d29c49f53f1e204c97e7af7bfb83d67
Ronny <ronnybrendel@gmail.com>
2013-07-24 11:05:10 +0200

    add stubs for double-slit experiment

---
+++ experiments/double-slit/main.cpp
@@ -0,0 +1,15 @@
+#include <QDebug>
+
+#include "complex.hpp"
+#include "complexvector.hpp"
+#include "complexmatrix.hpp"
+
+using namespace std;
+
+int main(int argc, char **args) {
+
+    (void) argc; (void) args;
+
+    return 0;
+}
+
