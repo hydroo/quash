ba36641a840712df1a7363cb2b66e6cc7bc00344
Ronny <ronnybrendel@gmail.com>
2013-07-26 17:29:35 +0200

    added State_Qubit_One, State_Qubit_Zero constants

--- libquash/state.hpp
+++ libquash/state.hpp
@@ -9,4 +9,7 @@ State State_probabilities(const State& s);
 
 Complex State_transitionAmpltitude(const State& fromKet, const State& toKet);
 
+const State State_Qubit_One = ComplexVector_fromString("[0, 1]");
+const State State_Qubit_Zero = ComplexVector_fromString("[1, 0]");
+
 #endif
